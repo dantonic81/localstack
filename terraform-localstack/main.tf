8f70edfdf2e7d98e25393f95d9c734d6538aa318
Dean <imadeureadis@gmail.com>
2025-02-04 16:15:46 +0100

    add tf main file

---
+++ terraform-localstack/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-bucket"
+}
+
+resource "aws_s3_bucket_acl" "my_bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.my_bucket.bucket
+}
